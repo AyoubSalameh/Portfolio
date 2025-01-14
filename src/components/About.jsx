3e48bcf7b0fd01b0521099e5ea2210f8c9cc3fa1
ayoubsalameh <salamehayoub@gmail.com>
2025-01-14 20:22:47 +0200

    added an about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,23 @@
 import './About.css';
-import { Col, Container, Row } from "react-bootstrap";
-import { FaLaptopCode } from 'react-icons/fa';
+import heroImg from '../assets/images/hero-img.png';
+import { Col, Container, Row, Button } from "react-bootstrap";
 
 function About() {
     return(
         <section id='about' className='about'>
             <Container>
                 <Row className="align-items-center">
-                    <Col className="d-flex"r>
-                        <FaLaptopCode size={100}/>
+                    <Col className="d-flex justify-content-center align-items-center" >
+                        <img src={heroImg} alt='hero' className='hero-pic'/>
                     </Col>
                     <Col xs={12} md={10} lg={8} className="text-center">
                         <h3>About Me</h3>
-                        <h4>Hi! im Ayoub Salameh, im a Computer Science Graduate from the Technion, and an aspiring Software Engineer
-                            with a passion for web development.
-                            i am currently looking for a full-time position as a Junior Software Engineer.
+                        <h4>Hi! I'm Ayoub Salameh, a Computer Science Graduate from the Technion, and an aspiring Software Engineer.
+                            I am currently looking for a full-time position as a Junior Software Engineer.
                         </h4>
+                        <Button href="/Portfolio/Ayoub.pdf" download className='resume-btn'>
+                            My Resume
+                        </Button>
                     </Col>
                 </Row>
             </Container>
